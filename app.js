f5b9851493d026f54d912e6177663c55e14d7e02
Richard Balogh <rbalogh03@gmail.com>
2022-03-03 20:26:35 +0100

    init

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 class DrumKit {
     constructor(){
         this.beats = document.querySelectorAll(".beat");
+        this.mutes = document.querySelectorAll(".mute")
     }
     activeBeat() {
+        this.classList.toggle("active")
+    }
+    mutedNote() {
         this.classList.toggle("active")
     }
 }
@@ -11,11 +15,18 @@ const drumKit = new DrumKit()
 
 drumKit.beats.forEach(beat=> {
     beat.addEventListener("click", function() {
-        console.log(this)
         drumKit.activeBeat
+        console.log(this)
         beat.style.animation = `beatDrop 0.3s alternate ease-in-out 2`;
         beat.addEventListener("animationend", function() {
             beat.style.animation = ""
         })
     });
+})
+
+drumKit.mutes.forEach(mute => {
+    mute.addEventListener("click",function(){
+        console.log(this)
+        drumKit.mutedNote
+    })
 })
